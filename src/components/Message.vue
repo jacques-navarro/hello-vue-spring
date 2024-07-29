a0895c2ca78e06e736fe18a2888fb806809e7d38
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-07-29 22:14:20 +0200

    Add Message component

---
+++ src/components/Message.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Message: {{ message }}</h1>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  },
+
+  methods: {
+    getData() {
+      axios.get("http://localhost:8080/hello").then((response) => {
+        this.message = response.data;
+      });
+    },
+  },
+
+  mounted() {
+    this.getData();
+  },
+};
+</script>
+
+<style></style>
